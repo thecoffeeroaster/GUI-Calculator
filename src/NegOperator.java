fd14e296373491a1a191d65d48f5e2b01c234475
smloehr <smloehr@asu.edu>
2022-02-20 21:27:30 -0700

    Neg Operator complete

--- src/NegOperator.java
+++ src/NegOperator.java
@@ -12,4 +12,34 @@
 /**
  * Represents the negation operator which is a specific type of unary operator.
  */
-???
+
+public class NegOperator extends UnaryOperator
+{
+    public NegOperator()
+    {
+
+    }
+    @Override
+    public Operand evaluate(Operand pOperand)
+    {
+        return new Operand(-pOperand.getValue());
+    }
+
+    /**
+     * Returns the normal precedence level of this operator.
+     */
+    @Override
+    public int precedence()
+    {
+        return 4;
+    }
+
+    /**
+     * Returns the precedence level of this operator when on it is on the operator stack.
+     */
+    @Override
+    public int stackPrecedence()
+    {
+        return 4;
+    }
+}
