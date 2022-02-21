10a52f675877c4d4a198966b1f8fbfceb21f794c
smloehr <smloehr@asu.edu>
2022-02-20 21:08:52 -0700

    Div Operator complete

--- src/DivOperator.java
+++ src/DivOperator.java
@@ -12,4 +12,30 @@
 /**
  * Represents the division operator which is a specific type of binary operator.
  */
-???
+
+public class DivOperator extends BinaryOperator
+{
+    @Override
+    public Operand evaluate(Operand pLhsOperand, Operand pRhsOperand)
+    {
+        return new Operand(pLhsOperand.getValue() + pRhsOperand.getValue());
+    }
+
+    /**
+     * Returns the normal precedence level of this operator.
+     */
+    @Override
+    public int precedence()
+    {
+        return 3;
+    }
+
+    /**
+     * Returns the precedence level of this operator when on it is on the operator stack.
+     */
+    @Override
+    public int stackPrecedence()
+    {
+        return 3;
+    }
+}
