cc95bb1a6992c3664db24348831f732f46bd2333
smloehr <smloehr@asu.edu>
2022-02-20 21:21:53 -0700

    Sub Operator complete

--- src/SubOperator.java
+++ src/SubOperator.java
@@ -12,4 +12,30 @@
 /**
  * Represents the subtraction operator which is a specific type of binary operator.
  */
-???
+
+public class SubOperator extends BinaryOperator
+{
+    @Override
+    public Operand evaluate(Operand pLhsOperand, Operand pRhsOperand)
+    {
+        return new Operand(pLhsOperand.getValue() - pRhsOperand.getValue());
+    }
+
+    /**
+     * Returns the normal precedence level of this operator.
+     */
+    @Override
+    public int precedence()
+    {
+        return 2;
+    }
+
+    /**
+     * Returns the precedence level of this operator when on it is on the operator stack.
+     */
+    @Override
+    public int stackPrecedence()
+    {
+        return 2;
+    }
+}
